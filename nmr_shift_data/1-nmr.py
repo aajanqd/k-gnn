e0183d9a9648eb78e99aea0cb42d2e184e657261
Aajan Quail <aqd215@nyu.edu>
2020-07-01 11:11:03 -0700

    trying more stuff with rdkit import error

--- nmr_shift_data/1-nmr.py
+++ nmr_shift_data/1-nmr.py
@@ -1,17 +1,17 @@
 import rdkit
 
-# import os.path as osp
+import os.path as osp
 import graph_conv_many_nuc_util
-# from graph_conv_many_nuc_util import move
-# import argparse
-# import torch
-# from torch.nn import Sequential, Linear, ReLU
-# import torch.nn.functional as F
-# from torch_scatter import scatter_mean
-# from torch_geometric.datasets import QM9
-# import torch_geometric.transforms as T
-# from torch_geometric.nn import NNConv
-# from torch_geometric.data import DataLoader
+from graph_conv_many_nuc_util import move
+import argparse
+import torch
+from torch.nn import Sequential, Linear, ReLU
+import torch.nn.functional as F
+from torch_scatter import scatter_mean
+from torch_geometric.datasets import QM9
+import torch_geometric.transforms as T
+from torch_geometric.nn import NNConv
+from torch_geometric.data import DataLoader
 
 # infile = '/scratch/aqd215/k-gnn/nmr_shift_data/graph_conv_many_nuc_pipeline.datasets/graph_conv_many_nuc_pipeline.data.13C.nmrshiftdb_hconfspcl_nmrshiftdb.aromatic.64.0.mol_dict.pickle'
 
