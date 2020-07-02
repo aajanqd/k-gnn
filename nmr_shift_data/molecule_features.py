09be6676ab5d3c2623a48839d4b973049cbbc6af
Aajan Quail <aqd215@nyu.edu>
2020-07-02 08:14:54 -0700

    rdkit import no longer an error

--- nmr_shift_data/molecule_features.py
+++ nmr_shift_data/molecule_features.py
@@ -140,8 +140,8 @@ def get_edge_attr_and_ind(m):
     edge_attr = torch.tensor([single, double, triple, aromatic],
                              dtype=torch.float).t().contiguous()
 
-    # print(tuple(edge_index.size()))
-    # print(tuple(edge_attr.size()))
+    print(tuple(edge_index.size()))
+    print(tuple(edge_attr.size()))
     # assert tuple(edge_index.size()) == (2,140)
     # assert tuple(edge_attr.size()) == (4,140)
 
