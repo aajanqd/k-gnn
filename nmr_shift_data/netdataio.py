017305d4867bd8578fd7349e9be196454cbb4cdc
Aajan Quail <aqd215@nyu.edu>
2020-07-02 07:20:09 -0700

    rdkit import no longer an error

--- nmr_shift_data/netdataio.py
+++ nmr_shift_data/netdataio.py
@@ -200,7 +200,7 @@ class MoleculeDatasetMulti(torch.utils.data.Dataset):
                 mask[int(k), pn] = 1.0
                 vals[int(k), pn] = v
 
-        v = (vect_feat, vals, edge_index, edge_attr)
+        v = (vect_feat, vals, edge_attr)
         
         
         # self.cache[self.cache_key(idx, conf_idx)] = v
