5b24fcd44cd8c61db9488ef730b5c8205de62110
Aajan Quail <aqd215@nyu.edu>
2020-07-25 15:59:49 -0700

    checking loss function

--- nmr_shift_data/loss_functions.py
+++ nmr_shift_data/loss_functions.py
@@ -1,5 +1,5 @@
 import torch
-import torch.nn as nn
+from torch.nn.functional import mse_loss
 import sys
 
 def MSE_loss(x,y,mask):
@@ -7,7 +7,7 @@ def MSE_loss(x,y,mask):
     sys.stdout.flush()
     x_masked = x[mask>0].reshape(-1, 1)
     y_masked = y[mask>0].reshape(-1, 1)
-    return nn.MSELoss(x_masked, y_masked)
+    return mse_loss(x_masked, y_masked)
 
 def MAE_loss(x,y,mask):
     x_masked = x[mask>0].reshape(-1, 1)
