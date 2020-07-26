4d84f896602d28702b09ea2d1de19de59001cadd
Aajan Quail <aqd215@nyu.edu>
2020-07-25 21:53:19 -0700

    final touches

--- nmr_shift_data/loss_functions.py
+++ nmr_shift_data/loss_functions.py
@@ -11,5 +11,5 @@ def MAE_loss(x,y,mask):
     x_masked = x[mask>0].reshape(-1, 1)
     y_masked = y[mask>0].reshape(-1, 1)
     l = y[mask>0].reshape(-1).size()[0]
-    MAE = float(abs(t1-t2).sum()/l)
+    MAE = float(abs(x_masked-y_masked).sum()/l)
     return MAE
