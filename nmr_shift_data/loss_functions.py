7cfac9a0d8523b8b45aab6f9d04fb944e8f9632a
Aajan Quail <aqd215@nyu.edu>
2020-07-28 12:01:43 -0700

    removed scatter mean (works)

--- nmr_shift_data/loss_functions.py
+++ nmr_shift_data/loss_functions.py
@@ -3,14 +3,14 @@ from torch.nn.functional import mse_loss
 import sys
 
 def MSE_loss(x,y,mask):
-    print("x, y, mask sizes: " +str(x.size())+","+str(y.size())+","+str(mask.size()))
+    # print("x, y, mask sizes: " +str(x.size())+","+str(y.size())+","+str(mask.size()))
     sys.stdout.flush()
     x_masked = x[mask>0]
     y_masked = y[mask>0]
     return mse_loss(x_masked, y_masked)
 
 def MAE_loss(x,y,mask):
-    print("x, y, mask sizes: " +str(x.size())+","+str(y.size())+","+str(mask.size()))
+    # print("x, y, mask sizes: " +str(x.size())+","+str(y.size())+","+str(mask.size()))
     sys.stdout.flush()
     x_masked = x[mask>0]
     y_masked = y[mask>0]
