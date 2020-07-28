78d44957e53d8b61b38f58923377b1e4093b76f4
Aajan Quail <aqd215@nyu.edu>
2020-07-28 11:31:36 -0700

    changing output tensor shape

--- nmr_shift_data/loss_functions.py
+++ nmr_shift_data/loss_functions.py
@@ -3,11 +3,15 @@ from torch.nn.functional import mse_loss
 import sys
 
 def MSE_loss(x,y,mask):
+    print("x, y, mask sizes: " +str(pred.size())+","+str(target.size())+","+str(mask.size()))
+    sys.stdout.flush()
     x_masked = x[mask>0]
     y_masked = y[mask>0]
     return mse_loss(x_masked, y_masked)
 
 def MAE_loss(x,y,mask):
+	print("x, y, mask sizes: " +str(pred.size())+","+str(target.size())+","+str(mask.size()))
+    sys.stdout.flush()
     x_masked = x[mask>0]
     y_masked = y[mask>0]
     l = y[mask>0].size()[0]
