66f8993ae6dd2f58d2d808430972ff7a488ceb35
Aajan Quail <aqd215@nyu.edu>
2020-08-03 17:13:22 -0700

    why such large initial training loss?

--- nmr_shift_data/1-nmr-constantLR.py
+++ nmr_shift_data/1-nmr-constantLR.py
@@ -14,6 +14,8 @@ import sys
 from loader_processing import process
 import loss_functions
 
+torch.set_printoptions(profile="full")
+
 infile = '/scratch/aqd215/k-gnn/nmr_shift_data/graph_conv_many_nuc_pipeline.datasets/graph_conv_many_nuc_pipeline.data.13C.nmrshiftdb_hconfspcl_nmrshiftdb.aromatic.64.0.mol_dict.pickle'
                                                                
 train_loader, val_loader, test_loader = process(infile)
