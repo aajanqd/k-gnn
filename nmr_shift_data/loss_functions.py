59152eda84e3331daea4e28517cea4ded05cc08e
Aajan Quail <aqd215@nyu.edu>
2020-08-07 15:42:37 -0700

    Trying Resnet structure

--- nmr_shift_data/loss_functions.py
+++ nmr_shift_data/loss_functions.py
@@ -5,10 +5,10 @@ import sys
 def MSE_loss(x,y,mask):
     x_masked = x[mask>0]
     y_masked = y[mask>0]
-    return ((x_masked-y_masked)**2).sum().item()
+    return mse_loss(x_masked, y_masked, reduction = 'sum')
 
 def MAE_loss(x,y,mask):
     x_masked = x[mask>0]
     y_masked = y[mask>0]
-    MAE = abs(x_masked-y_masked).sum().item()
+    MAE = float(abs(x_masked-y_masked).sum())
     return MAE
