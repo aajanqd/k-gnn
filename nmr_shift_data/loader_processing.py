6306f89db9594fdf443ab9b9a50707dd92464a25
Aajan Quail <aqd215@nyu.edu>
2020-08-06 22:14:41 -0700

    playing around with structures and learning rates

--- nmr_shift_data/loader_processing.py
+++ nmr_shift_data/loader_processing.py
@@ -63,15 +63,15 @@ def process(infile):
     print('made dataset')
     sys.stdout.flush()
 
-    train_split = int(len(dataset)*0.3)
-    val_split = int(len(dataset)*0.1)
+    train_split = int(len(dataset)*0.6)
+    val_split = int(len(dataset)*0.2)
     train_dataset = dataset[:train_split]
     val_dataset = dataset[train_split:train_split+val_split]
-    test_dataset = dataset[train_split+val_split:train_split+2*val_split]
+    test_dataset = dataset[train_split+val_split:]
 
-    train_loader = DataLoader(train_dataset, batch_size=32, num_workers=1)
-    val_loader = DataLoader(val_dataset, batch_size=32, num_workers=1)
-    test_loader = DataLoader(test_dataset, batch_size=32, num_workers=1)
+    train_loader = DataLoader(train_dataset, batch_size=8, num_workers=1)
+    val_loader = DataLoader(val_dataset, batch_size=8, num_workers=1)
+    test_loader = DataLoader(test_dataset, batch_size=8, num_workers=1)
     print('created data loaders')
     sys.stdout.flush()
 
